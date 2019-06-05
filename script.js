3ab9228dd3caef610c137e99f418e0557b669166
William <whuynh23@gmail.com>
2019-06-05 00:20:28 -0700

    couldn't fix

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function(){
         hamburger.classList.toggle("is-active");
         nav.classList.toggle("is-active");
         document.querySelector("#main-container").classList.toggle("blur");
+        background.classList.add("blur");
     });
     const calculatedHeight = window.getComputedStyle(background).getPropertyValue("height");
     const calculatedWidth = window.getComputedStyle(background).getPropertyValue("width");
